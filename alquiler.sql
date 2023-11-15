8eb5dbb9c95fa025524dfdbef3a75be37707b991
Angel Sifuentes <angelsifuentes2580@gmail.com>
2023-11-15 11:43:08 -0500

    Add database

--- alquiler.sql
+++ alquiler.sql
@@ -1,150 +1,185 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
--- SO del servidor:              Win64
--- HeidiSQL Versión:             12.1.0.6537
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-11-2023 a las 17:40:50
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Volcando estructura para tabla alquiler.alquiler
-CREATE TABLE IF NOT EXISTS `alquiler` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `cantidad` int NOT NULL,
-  `tipo_precio` int NOT NULL DEFAULT '0',
+--
+-- Base de datos: `alquiler`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alquiler`
+--
+
+CREATE TABLE `alquiler` (
+  `id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `tipo_precio` int(11) NOT NULL DEFAULT 0,
   `monto` decimal(10,2) NOT NULL,
   `abono` decimal(10,2) NOT NULL,
   `fecha_prestamo` datetime NOT NULL,
   `fecha_devolucion` datetime NOT NULL,
-  `observacion` text,
-  `estado` int NOT NULL DEFAULT '1',
-  `id_cliente` int NOT NULL,
-  `id_vehiculo` int NOT NULL,
-  `id_doc` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_cliente` (`id_cliente`),
-  KEY `id_vehiculo` (`id_vehiculo`),
-  KEY `id_doc` (`id_doc`),
-  CONSTRAINT `alquiler_ibfk_1` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `alquiler_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `alquiler_ibfk_3` FOREIGN KEY (`id_doc`) REFERENCES `documentos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `observacion` text DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `id_cliente` int(11) NOT NULL,
+  `id_vehiculo` int(11) NOT NULL,
+  `id_doc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.alquiler: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla alquiler.clientes
-CREATE TABLE IF NOT EXISTS `clientes` (
-  `id` int NOT NULL AUTO_INCREMENT,
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
   `dni` varchar(10) DEFAULT NULL,
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(100) DEFAULT NULL,
   `codphone` varchar(10) DEFAULT NULL,
-  `telefono` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `telefono` varchar(20) NOT NULL,
   `direccion` text NOT NULL,
   `clave` varchar(150) DEFAULT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `estado` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `estado` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.clientes: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla alquiler.configuracion
-CREATE TABLE IF NOT EXISTS `configuracion` (
-  `id` int NOT NULL AUTO_INCREMENT,
+--
+-- Estructura de tabla para la tabla `configuracion`
+--
+
+CREATE TABLE `configuracion` (
+  `id` int(11) NOT NULL,
   `ruc` varchar(20) NOT NULL,
   `nombre` varchar(200) NOT NULL,
   `telefono` varchar(15) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `direccion` varchar(200) NOT NULL,
   `mensaje` text NOT NULL,
-  `logo` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `logo` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.configuracion: ~1 rows (aproximadamente)
+--
+-- Volcado de datos para la tabla `configuracion`
+--
+
 INSERT INTO `configuracion` (`id`, `ruc`, `nombre`, `telefono`, `correo`, `direccion`, `mensaje`, `logo`) VALUES
-	(1, '12345678910', 'Vida Informático', '963852147', 'programadornaju@gmail.com', 'LIMA PERU', 'GRACIAS POR SU PREFERENCIA', 'logo.png');
+(1, '12345678910', 'Vida Informático', '963852147', 'programadornaju@gmail.com', 'LIMA PERU', 'GRACIAS POR SU PREFERENCIA', 'logo.png');
 
--- Volcando estructura para tabla alquiler.documentos
-CREATE TABLE IF NOT EXISTS `documentos` (
-  `id` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documentos`
+--
+
+CREATE TABLE `documentos` (
+  `id` int(11) NOT NULL,
   `documento` varchar(20) NOT NULL,
-  `estado` int NOT NULL DEFAULT '1',
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.documentos: ~2 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla alquiler.marcas
-CREATE TABLE IF NOT EXISTS `marcas` (
-  `id` int NOT NULL AUTO_INCREMENT,
+--
+-- Estructura de tabla para la tabla `marcas`
+--
+
+CREATE TABLE `marcas` (
+  `id` int(11) NOT NULL,
   `marca` varchar(50) NOT NULL,
-  `estado` int NOT NULL DEFAULT '1',
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.marcas: ~8 rows (aproximadamente)
+--
+-- Volcado de datos para la tabla `marcas`
+--
+
 INSERT INTO `marcas` (`id`, `marca`, `estado`, `fecha`) VALUES
-	(1, 'FERRARI', 1, '2021-11-11 18:45:57'),
-	(2, 'HONDA', 1, '2021-11-11 18:46:06'),
-	(3, 'NISAN', 1, '2021-11-11 18:46:19'),
-	(4, 'TOYOTA', 1, '2021-11-11 18:46:28'),
-	(5, 'VOLVO', 1, '2021-11-11 18:46:38'),
-	(6, 'SUZUKI', 1, '2021-11-11 18:46:53'),
-	(7, 'MORGAN', 1, '2021-11-11 18:47:22'),
-	(8, 'TESLA', 1, '2021-11-11 18:47:30');
+(1, 'FERRARI', 1, '2021-11-11 18:45:57'),
+(2, 'HONDA', 1, '2021-11-11 18:46:06'),
+(3, 'NISAN', 1, '2021-11-11 18:46:19'),
+(4, 'TOYOTA', 1, '2021-11-11 18:46:28'),
+(5, 'VOLVO', 1, '2021-11-11 18:46:38'),
+(6, 'SUZUKI', 1, '2021-11-11 18:46:53'),
+(7, 'MORGAN', 1, '2021-11-11 18:47:22'),
+(8, 'TESLA', 1, '2021-11-11 18:47:30');
 
--- Volcando estructura para tabla alquiler.reservas
-CREATE TABLE IF NOT EXISTS `reservas` (
-  `id` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(11) NOT NULL,
   `f_recogida` datetime NOT NULL,
   `f_entrega` datetime NOT NULL,
-  `cantidad` int NOT NULL DEFAULT '0',
-  `tipo_precio` int NOT NULL DEFAULT '1',
-  `monto` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `cantidad` int(11) NOT NULL DEFAULT 0,
+  `tipo_precio` int(11) NOT NULL DEFAULT 1,
+  `monto` decimal(10,2) NOT NULL DEFAULT 0.00,
   `f_reserva` datetime NOT NULL,
-  `observacion` text CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci,
-  `estado` int NOT NULL DEFAULT '0',
-  `id_vehiculo` int NOT NULL,
-  `id_cliente` int NOT NULL,
-  PRIMARY KEY (`id`)
+  `observacion` text DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT 0,
+  `id_vehiculo` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla alquiler.reservas: ~2 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla alquiler.tipos
-CREATE TABLE IF NOT EXISTS `tipos` (
-  `id` int NOT NULL AUTO_INCREMENT,
+--
+-- Estructura de tabla para la tabla `tipos`
+--
+
+CREATE TABLE `tipos` (
+  `id` int(11) NOT NULL,
   `tipo` varchar(50) NOT NULL,
-  `estado` int NOT NULL DEFAULT '1',
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.tipos: ~7 rows (aproximadamente)
+--
+-- Volcado de datos para la tabla `tipos`
+--
+
 INSERT INTO `tipos` (`id`, `tipo`, `estado`, `fecha`) VALUES
-	(1, 'CAMIONETA', 1, '2021-11-11 14:44:50'),
-	(2, 'MINI BAN', 1, '2021-11-11 14:46:20'),
-	(3, 'MOTOCICLETA', 1, '2021-11-11 18:48:32'),
-	(4, 'MOTO CARRO', 1, '2021-11-11 18:48:55'),
-	(5, 'TURISMO', 1, '2021-11-11 18:49:13'),
-	(6, 'CAMION', 1, '2021-11-11 18:49:21'),
-	(7, 'Furgón', 1, '2021-11-11 18:49:54'),
-	(8, 'Mercedes Grand Sedan', 1, '2023-11-09 18:40:29');
+(1, 'CAMIONETA', 1, '2021-11-11 14:44:50'),
+(2, 'MINI BAN', 1, '2021-11-11 14:46:20'),
+(3, 'MOTOCICLETA', 1, '2021-11-11 18:48:32'),
+(4, 'MOTO CARRO', 1, '2021-11-11 18:48:55'),
+(5, 'TURISMO', 1, '2021-11-11 18:49:13'),
+(6, 'CAMION', 1, '2021-11-11 18:49:21'),
+(7, 'Furgón', 1, '2021-11-11 18:49:54'),
+(8, 'Mercedes Grand Sedan', 1, '2023-11-09 18:40:29');
 
--- Volcando estructura para tabla alquiler.usuarios
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) DEFAULT NULL,
@@ -152,45 +187,193 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `telefono` varchar(20) DEFAULT NULL,
   `direccion` varchar(100) DEFAULT NULL,
   `perfil` varchar(50) NOT NULL DEFAULT 'avatar.svg',
-  `clave` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `estado` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `clave` varchar(150) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `estado` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.usuarios: ~3 rows (aproximadamente)
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
 INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `apellido`, `correo`, `telefono`, `direccion`, `perfil`, `clave`, `fecha`, `estado`) VALUES
-	(1, 'ADMIN', 'VIDA INFORMÁTICO', 'SITIO WEB', 'admin@angelsifuentes.com', '925491523', 'HUARI - ANCASH', '20231114201652.jpg', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '2023-11-15 16:17:19', 1);
+(1, 'ADMIN', 'VIDA INFORMÁTICO', 'SITIO WEB', 'admin@angelsifuentes.com', '925491523', 'HUARI - ANCASH', '20231114201652.jpg', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '2023-11-15 16:17:19', 1);
 
--- Volcando estructura para tabla alquiler.vehiculos
-CREATE TABLE IF NOT EXISTS `vehiculos` (
-  `id` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculos`
+--
+
+CREATE TABLE `vehiculos` (
+  `id` int(11) NOT NULL,
   `placa` varchar(50) NOT NULL,
-  `precio_hora` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `precio_dia` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `precio_mes` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `precio_hora` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `precio_dia` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `precio_mes` decimal(10,2) NOT NULL DEFAULT 0.00,
   `modelo` varchar(50) NOT NULL,
   `kilometraje` varchar(50) NOT NULL,
   `transmision` varchar(50) NOT NULL,
-  `asientos` int NOT NULL DEFAULT '0',
+  `asientos` int(11) NOT NULL DEFAULT 0,
   `equipaje` varchar(50) NOT NULL,
   `combustible` varchar(50) NOT NULL,
   `foto` varchar(50) NOT NULL,
-  `estado` int NOT NULL DEFAULT '1',
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_tipo` int NOT NULL,
-  `id_marca` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_marca` (`id_marca`),
-  KEY `id_tipo` (`id_tipo`),
-  CONSTRAINT `vehiculos_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `vehiculos_ibfk_2` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_tipo` int(11) NOT NULL,
+  `id_marca` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla alquiler.vehiculos: ~0 rows (aproximadamente)
+--
+-- Índices para tablas volcadas
+--
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Indices de la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_vehiculo` (`id_vehiculo`),
+  ADD KEY `id_doc` (`id_doc`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `configuracion`
+--
+ALTER TABLE `configuracion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `marcas`
+--
+ALTER TABLE `marcas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_vehiculo` (`id_vehiculo`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `tipos`
+--
+ALTER TABLE `tipos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_marca` (`id_marca`),
+  ADD KEY `id_tipo` (`id_tipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `configuracion`
+--
+ALTER TABLE `configuracion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `marcas`
+--
+ALTER TABLE `marcas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipos`
+--
+ALTER TABLE `tipos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD CONSTRAINT `alquiler_ibfk_1` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `alquiler_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `alquiler_ibfk_3` FOREIGN KEY (`id_doc`) REFERENCES `documentos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD CONSTRAINT `vehiculos_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `vehiculos_ibfk_2` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
